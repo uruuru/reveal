6e0e8e26ce1acbfacc1bc8a02ef75b01002f49d3
Ulf Rüegg <uruurumail@gmail.com>
2025-01-14 18:37:03 +0100

    Add initial set of objects

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ This template should help get you started developing with Tauri in vanilla HTML,
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+
+
+
+# State and Exchanged Objects
+
+* `RevealState` 
+  * Current list of images
+  * "Active" image
+
+* `RevealSettings`
+  * Image source (directory, gallery, ...)
+  * Covering type (triangles, rectangles, ...)
+  * Uncovering strategy (manual, automatic, ...)
+  * Show control buttons
+
+* `RevealObject`
+  * Image
+  * Covering
+  * Question
+    * Answer options
+    * Correct answer
+  * Settings
---
+++ src-tauri/src/common.rs
@@ -0,0 +1,67 @@
+use serde::Serialize;
+use std::path::PathBuf;
+use ts_rs::TS;
+
+pub struct RevealState {
+    pub images: Vec<PathBuf>,
+    pub image_index: usize,
+}
+
+#[derive(Default, Serialize, TS)]
+#[ts(export)]
+pub enum CoveringType {
+    #[default]
+    Triangles,
+    Rectangles,
+}
+
+#[derive(Default, Serialize, TS)]
+#[ts(export)]
+pub enum UncoveringStrategy {
+    #[default]
+    Manual,
+}
+
+#[derive(Serialize, TS)]
+#[ts(export)]
+pub struct RevealSettings {
+    pub image_source: Option<PathBuf>,
+    pub covering_type: CoveringType,
+    pub uncovering_strategy: UncoveringStrategy,
+    pub show_control_buttons: bool,
+}
+
+impl Default for RevealSettings {
+    fn default() -> Self {
+        RevealSettings {
+            image_source: None,
+            covering_type: CoveringType::Triangles,
+            uncovering_strategy: UncoveringStrategy::Manual,
+            show_control_buttons: true,
+        }
+    }
+}
+
+#[derive(Default, Serialize, TS)]
+#[ts(export)]
+pub struct RevealObject {
+    /// Base64 encoded image
+    pub image: String,
+    /// MIME subtype of the image, e.g. 'png' or 'webp'
+    pub image_type: String,
+    pub covering: String, // Custom format
+    pub question: Option<String>,
+    pub answers: Vec<String>,
+    pub correct_answer: usize,
+}
+
+#[derive(Debug)]
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
+#[derive(Debug)]
+pub struct Polygon {
+    pub pnts: Vec<Point>,
+}
