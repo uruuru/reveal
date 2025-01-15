7d87c7212d14791295b3402e79fdc4cde59f8b3b
Ulf Rüegg <uruurumail@gmail.com>
2025-01-15 20:51:55 +0100

    Set log level to debug

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -33,7 +33,11 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(log::LevelFilter::Debug)
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![debug_infos, get_settings, example])
         .run(tauri::generate_context!())
