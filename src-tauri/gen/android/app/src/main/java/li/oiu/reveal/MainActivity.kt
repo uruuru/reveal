be86f6028bfaac6899b8951abdbd2874b819db52
Ulf Rüegg <uruurumail@gmail.com>
2025-01-16 21:36:17 +0100

    android: request access to storage

--- src-tauri/gen/android/app/src/main/java/li/oiu/reveal/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/li/oiu/reveal/MainActivity.kt
@@ -1,3 +1,58 @@
 package li.oiu.reveal
 
-class MainActivity : TauriActivity()
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : TauriActivity() {
+    
+    private val STORAGE_PERMISSION_CODE = 100
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Manifest.permission.READ_MEDIA_IMAGES
+        } else {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        }
+
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                AlertDialog.Builder(this)
+                    .setTitle("Access to images")
+                    .setMessage("The app needs access to your images.")
+                    .setPositiveButton("Ok") { _, _ ->
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(permission),
+                            STORAGE_PERMISSION_CODE
+                        )
+                    }
+                    .create()
+                    .show()
+            } else {
+                // For Android 5.1 and below
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(permission),
+                    STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+    }
+    
+    // override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    //     super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    //     if (requestCode == STORAGE_PERMISSION_CODE && grantResults.isNotEmpty()) {
+    //         if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+    //             // Nothing at the moment.
+    //         }
+    //     }
+    // }
+}
