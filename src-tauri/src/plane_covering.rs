0d465580c72d50ce340aa499c4f91c06f4b17615
Ulf Rüegg <uruurumail@gmail.com>
2025-01-21 19:28:52 +0100

    Ensure image is fully covered.

--- src-tauri/src/plane_covering.rs
+++ src-tauri/src/plane_covering.rs
@@ -2,8 +2,8 @@ use crate::common::{Point, Polygon};
 
 pub fn cover_rectangles(n: usize, w: f64, h: f64) -> Vec<Polygon> {
     let rows_cols = n.isqrt();
-    let row_step: f64 = h.div_euclid(rows_cols as f64);
-    let col_step: f64 = w.div_euclid(rows_cols as f64);
+    let row_step: f64 = h / rows_cols as f64;
+    let col_step: f64 = w / rows_cols as f64;
     itertools::iproduct!(0..rows_cols, 0..rows_cols)
         .map(|(row, col)| Polygon {
             pnts: vec![
@@ -27,4 +27,3 @@ pub fn cover_rectangles(n: usize, w: f64, h: f64) -> Vec<Polygon> {
         })
         .collect()
 }
-
