176f6e95510cf7dcd1bc86c7364709ced314713c
Ulf Rüegg <uruurumail@gmail.com>
2025-01-22 21:34:37 +0100

    Limit quiz answer possibilities to the current year.

--- src-tauri/src/questions.rs
+++ src-tauri/src/questions.rs
@@ -9,13 +9,16 @@ pub struct QuestionAndAnswers {
 }
 
 pub fn simple_year_question(date: &NaiveDateTime) -> QuestionAndAnswers {
-    let year = date.date().year();
-    let offset = rand::thread_rng().gen_range(0..2);
+    let year_taken = date.date().year();
+    let year_now = chrono::Utc::now().year();
+    let number_of_answers = 3;
+    let max_offset = std::cmp::min(number_of_answers - 1, year_now - year_taken);
+    let offset = rand::thread_rng().gen_range(0..=max_offset);
     QuestionAndAnswers {
         question: "Which year was the picture taken?".into(),
-        answers: (year - offset..year - offset + 3)
+        answers: (year_taken - (number_of_answers - 1) + offset..=year_taken + offset)
             .map(|i| i.to_string())
             .collect(),
-        idx_correct: offset as usize,
+        idx_correct: ((number_of_answers - 1) - offset) as usize,
     }
 }
